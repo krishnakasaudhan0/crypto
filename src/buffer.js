9303091c51d88fd6ec0012fdb0c4e07062f2649f
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-01-07 12:12:50 +0000

    refactor: modify buffer byte readers

--- src/buffer.js
+++ src/buffer.js
@@ -2,3 +2,5 @@
 // Git contribution logic update at: 2026-01-05T13:15:48.000Z [hash: e6pal]
 
 // Git contribution logic update at: 2026-01-05T08:50:41.000Z [hash: rnpg7b]
+
+// Git contribution logic update at: 2026-01-07T12:12:50.000Z [hash: 9pehu9]
