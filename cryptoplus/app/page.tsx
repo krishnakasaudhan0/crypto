57937a5f1d50ba5dd75634bf14284f43dfaafb3d
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-05-26 20:41:16 +0530

    implemented navigation

---
+++ cryptoplus/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>coinpulse  </p>
+  )
+}
+
+export default page
---
+++ cryptoplus/component/Header.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link"
+import Image from "next/image"
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+
+const Header = () => {
+  const pathName = usePathname();
+  return (
+
+    <header>
+        <div className="main-container inner">
+            <Link href="/">
+             <Image src=" logo.svg" alt="coinpulse" width={132} height={40}  />
+            </Link>
+
+            <nav>
+                <Link href="/" className={cn('nav-link',{
+                    'is-active': pathName === '/',
+                    'is-home': true
+                })}>Home</Link>
+                <p>Search Modal</p>
+                <Link href="/coins" className={cn('nav-link',{
+                    'is-active': pathName === '/coins',
+                   
+                })}>All Coins</Link>
+                
+            </nav>
+        </div>
+    </header>
+  )
+
+}
+
+export default Header
